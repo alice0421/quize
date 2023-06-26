38bd9bd9ea85363f0cbc5e328f5a578706b0c62c
有栖 <sekka.alice@gmail.com>
2023-06-26 12:33:37 +0900

    [add] リレーションをModelに記述。

---
+++ src/app/Models/QuizCategory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class QuizCategory extends Model
+{
+    use HasFactory;
+
+    public function quizzes()
+    {
+        return $this->hasMany(Quiz::class);
+    }
+}
