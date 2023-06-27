bb2a2c09ecd350cda27310866843ef93501c11f5
有栖 <sekka.alice@gmail.com>
2023-06-28 07:35:31 +0900

    [update] 既存ページのレイアウトやデザインの微調整。

--- src/resources/js/Components/Homes/ContentSelectButton.vue
+++ src/resources/js/Components/Homes/ContentSelectButton.vue
@@ -12,7 +12,7 @@ defineProps({
 </script>
 
 <template>
-    <a :href=href class="table whitespace-pre-wrap rounded-md h-24 w-56" :class=bg_color>
+    <a :href=href class="table whitespace-pre-wrap rounded-md h-24 w-80" :class=bg_color>
         <p class="table-cell text-center text-2xl align-middle py-2">
             <slot />
         </p>
--- src/resources/js/Layouts/MainLayout.vue
+++ src/resources/js/Layouts/MainLayout.vue
@@ -7,19 +7,19 @@ const showingNavigationDropdown = ref(false);
 
 <template>
     <div>
-        <div class="flex h-screen w-screen bg-gray-100">
+        <div class="flex h-screen w-screen bg-white">
             <!-- Page Sidebar -->
-            <menu class="w-60 border-r border-gray-500">
-                <div class="py-12">
-                    <SideBarContent :href="route('home')" class="mx-auto">ホーム</SideBarContent>
-                    <SideBarContent :href="route('dashboard')" class="mx-auto mt-4">学習状況</SideBarContent>
-                </div>
+            <menu class="flex-none w-60 h-full py-12 border-r border-gray-500">
+                <SideBarContent :href="route('home')" class="mx-auto">ホーム</SideBarContent>
+                <SideBarContent :href="route('dashboard')" class="mx-auto mt-4">学習状況</SideBarContent>
             </menu>
 
             <!-- Page Content -->
-            <main class="h-full w-full py-12">
-                <slot />
-            </main>
+            <div class="flex-1 h-full px-8 py-12">
+                <main class="w-[1000px] h-full mx-auto">
+                    <slot />
+                </main>
+            </div>
         </div>
     </div>
 </template>
