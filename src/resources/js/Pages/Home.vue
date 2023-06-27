2648c4f4d77136a7046401fbbae02820d3e88a55
有栖 <sekka.alice@gmail.com>
2023-06-27 19:49:08 +0900

    [add] ホーム画面を作成。
    ・ホーム画面のVueを修正。
    ・ホーム画面用のコンポーネントを作成。

--- src/resources/js/Pages/Home.vue
+++ src/resources/js/Pages/Home.vue
@@ -1,5 +1,6 @@
 <script setup>
 import MainLayout from '@/Layouts/MainLayout.vue';
+import SelectContent from '@/Components/Homes/SelectContent.vue';
 import { Head } from '@inertiajs/vue3';
 </script>
 
@@ -7,8 +8,12 @@ import { Head } from '@inertiajs/vue3';
     <Head title="Quiz" />
 
     <MainLayout>
-        <div class="py-12">
-            <div class="p-6 text-gray-900">Home!</div>
+        <h1 class="text-4xl text-center">ホーム</h1>
+
+        <div class="pt-10 pb-20 h-full flex flex-col justify-between items-center">
+            <selectContent bg_color="bg-orange-300">みんなで</selectContent>
+            <selectContent :href="route('quiz.select')" bg_color="bg-emerald-300">ひとりで</selectContent>
+            <selectContent :href="route('dashboard')" bg_color="bg-blue-300">学習状況</selectContent>
         </div>
     </MainLayout>
 </template>
